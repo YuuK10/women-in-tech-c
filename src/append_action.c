8f691451fc397293921e2282a6a7c13e5fe43e13
Jerome Carere <jecarere@e-r8-p1.s19.be>
2018-10-01 16:58:04 +0200

    created append_action.c

---
+++ src/append_action.c
@@ -0,0 +1,21 @@
+#include "game.h"
+
+void *append_action(t_action_type action, char *arg)
+{
+    t_game_action *elem;
+    if((elem = malloc(sizeof(t_game_action))) <= 0)
+        return;
+    elem->type = action;
+    elem->arg = arg;
+    elem->next = NULL;
+    if (*action_list == NULL)
+    {
+        last_action = elem;
+        action_list = last_action;
+    }
+    else
+    {
+        last_action->next = elem;
+        last_action = elem;
+    }
+}
