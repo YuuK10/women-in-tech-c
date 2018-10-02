03743a355f33adc862faee68d60bd8c54ac534e4
Loïc Siquet <loicsiquet@gmail.com>
2018-10-02 16:54:48 +0200

    Fixed t_level_element naming

--- includes/static.h
+++ includes/static.h
@@ -4,7 +4,7 @@
 extern t_game_action	*action_list;
 extern t_game_action	*last_action;
 
-extern t_level_list		**level_list;
+extern t_level_element	**level_list;
 extern char				*level_name;
 
 extern t_player player;
