34b4f4eecd923e0fd0d36c6614a34a61f6295ac3
lsiquet <lsiquet@student.s19.be>
2018-10-05 12:16:40 +0200

    Updated install.sh (to be tested on mac os)

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
+#!/bin/zsh
+
 echo "First compilation..."
 make all
 
 echo "Configuring scripts..."
 
 game_path=$(pwd)
-
 game_path_exp=$(sed 's/\//\\\//g' <<< $game_path)
-exp="s/.*__GAME_PATH__/game_path=\"${game_path_exp}\/\"/"
-sed -i "" $exp scripts/*
+expr="s/.*__GAME_PATH__/game_path=\'${game_path_exp}\/\'/"
+
+if [[ "$uname" == "Darwin" ]]; then
+	sed -i "" ${expr} scripts/*
+else
+	sed -i ${expr} scripts/*
+fi
 
 echo "Creating alias"
 echo "alias playgame='$game_path/scripts/playgame.sh'" >> ~/.zshrc
 echo "alias editcode='$game_path/scripts/editcode.sh'" >> ~/.zshrc
-echo "alias howtoplay='$game_path/game'" >> ~/.zshrc
+echo "alias howtoplay='$game_path/scripts/howtoplay.sh'" >> ~/.zshrc
+echo "alias nextlevel='$game_path/scripts/nextlevel.sh'" >> ~/.zshrc
+source ~/.zshrc
 reset
 echo "Installation complete."
