73382c25d3298e262a817539bfba56f5f8891d7f
lsiquet <lsiquet@student.s19.be>
2018-10-07 13:02:11 +0200

    Updated installation script

--- install.sh
+++ install.sh
@@ -15,11 +15,20 @@ else
 	sed -i ${expr} scripts/*
 fi
 
+echo "Creating saves directory..."
+mkdir data/saves
+
+echo "Initializing game..."
+rm src/player_function.c 2>&1 > /dev/null
+lvl=$(cat data/current_level)
+cp data/samples/$lvl.c src/player_function.c
+
 echo "Creating alias"
 echo "alias playgame='$game_path/scripts/playgame.sh'" >> ~/.zshrc
 echo "alias editcode='$game_path/scripts/editcode.sh'" >> ~/.zshrc
 echo "alias howtoplay='$game_path/scripts/howtoplay.sh'" >> ~/.zshrc
 echo "alias nextlevel='$game_path/scripts/nextlevel.sh'" >> ~/.zshrc
+echo "alias resetlevel='$game_path/scripts/resetlevel.sh'" >> ~/.zshrc
 source ~/.zshrc
 reset
 echo "Installation complete."
