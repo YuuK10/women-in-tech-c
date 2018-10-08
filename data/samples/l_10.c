149a45dc6a4403f1e5e747fff2e851dacf989e1c
Loic Siquet <lsiquet@e-r7-p4.s19.be>
2018-10-08 12:40:39 +0200

    Created levels 6, 7, 8, 9, 10 (Docs to be created)

---
+++ data/samples/l_10.c
@@ -0,0 +1,24 @@
+#include "game_functions.h"
+
+/*
+**	A loop is a piece of code that will be repeated, until a
+**	condition becomes false.
+**
+**	It is written like this :
+**	while (condition)
+**	{
+**		code to execute
+**	}
+**/
+
+void player_function()
+{
+	int iterator; //We declare a variable named iterator
+
+	iterator = 1; //We set the value of 1 to iterator
+	while (iterator <= 20) //While iterator is < or = 20, do the following code
+	{
+		//Maybe something is missing to complete the level...
+		iterator = iterator + 1;
+	}
+}
