02b7d5ea613010e9f70b6fdfe288f88e701facea
Loic Siquet <lsiquet@e-r7-p4.s19.be>
2018-10-08 18:33:51 +0200

    Hide compilation errors

--- scripts/playgame.sh
+++ scripts/playgame.sh
@@ -3,5 +3,11 @@ cd $game_path
 
 lvl=$(cat data/current_level)
 echo "Recompiling level ${lvl}, please wait..."
-make
-./game "${lvl}"
+errors=$(make 2>&1)
+
+if [ -z "$errors" ]
+then
+	./game "${lvl}"
+else
+	echo "\033[0;35mOh, it looks like you have errors in your code. You should recheck it :)"
+fi
