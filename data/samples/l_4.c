957d1a7a7a30b209393b5e37f5904a7920a44b2f
Julien Drion <judrion@e-r8-p3.s19.be>
2018-10-08 15:11:31 +0200

    update level_4 and level_5

--- data/samples/l_4.c
+++ data/samples/l_4.c
@@ -1,5 +1,31 @@
+/* 
+ * This is a commentary. Everything you write on this file will
+ * be used against you but everything isn't usefull to help
+ * Archimed getting out. As you'll progress through the game
+ * you'll learn concept of -coding- here it's the commentary.
+ *
+ * You can write something for other people who works on the same
+ * project. Helping them understand the codes you wrotes.
+ *
+ * you can put text between \/\* YOUR TEXTE HERE \*\/
+ * (without \ this is used to "escape" charcter next to it and 
+ * avoid having compilation trouble)
+ *
+ * You can delete multiple line at once in vim. To do that
+ * you need to type the number of line and press "d" to times
+ * (try "13dd" with the cursor on the first line)
+ * 
+ * pressing "d" two times without a number of line delete one line
+ *
+ */
+
 #include "game_functions.h"
 void player_function()
 {
-
+	walk(LEFT);
+	walk(LEFT);
+	walk(LEFT);
+	walk(LEFT);
+	walk(LEFT);
+	walk(LEFT);
 }
