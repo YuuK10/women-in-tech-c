a64f71b92b202d32d1b1a4f7e57f0170c14b9949
Loic Siquet <lsiquet@e-r7-p4.s19.be>
2018-10-10 09:57:14 +0200

    Level 12 and 13's doc Updated level 12's code sample

--- data/samples/en/l_12.c
+++ data/samples/en/l_12.c
@@ -1,5 +1,20 @@
 #include "game_functions.h"
 void player_function()
 {
-	
+	int i;
+	int j;
+
+	i = 1;
+	while (i < 2)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			walk(UP);
+			j++;
+		}
+		j = 0;
+		//Try to complete the code from here...
+		i++;
+	}
 }
