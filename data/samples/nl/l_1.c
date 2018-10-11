1941a0c2dd2f03db9740da122654c9dbd953a67e
Loic Siquet <lsiquet@w-r5-p3.s19.be>
2018-10-11 18:40:51 +0200

    Samples translation in NL

---
+++ data/samples/nl/l_1.c
@@ -0,0 +1,45 @@
+#include "game_functions.h"
+
+/*	Welkom in je eerste C programmeertaal bestand !
+**
+**	Wat je kan zien beneden is een functie. Een functie is een stuk code
+**  dat wordt uitgevoerd door het programma.
+**
+**	Je gaat de instructies voor Archimede moeten schrijven tussen { en }
+**  namelijk genoemd brackets. Je mag deze niet verwijderen anders gaat je code
+**  niet werken. Als dit gebeurd dan daagt er een "compilation-erreor" op.
+**
+**	We hebben al een functie geschreven namelijk:
+
+**			walk(direction)
+
+**	Het is de errste fiunctie dat je gaat moeten gebreuken. Het zegt tegen
+**  Archimede de uil dat ze 1 tap moet doen in de gegeven richting. Je moet de
+**  richting schrijven tussen ( en ) en in hoofdletters (LEFT, RIGHT, UP, DOWN).
+**
+**	-----------------------------------------------------------------------
+**	Je gebruikt voor het moment de VIM tekst editor.
+**	Om dit bestand aan te kunnen passen moet je op de knop I drukken. Op dat
+**	moment betreed je het "INSERT" mode (kijk beneden aan je scherm).
+**
+**	Om de "INSERT" mode te verlaten moet je op ESC drukken.
+**
+**	Om een bestand op te slagen en te verlaten moet je het "INSERT" mode
+**  verlaten (kijk of "-- INSERT --" nog onderaan je scherm staat), en typ
+**  ":x" (zonder de dubbele quotes en ja met de 2 puntjes) en druk ENTER.
+**	-----------------------------------------------------------------------
+**
+**	Een laatste punt : elke C instructie moet geeïndigd zijn met een semicolon
+**  (;) om geldig te zijn.
+**
+**	Misschien moet je nu dit bestand verlaten en de commando "playgame" typen
+**  om te zien wat de al geschreven functie doet. Kom daarna terug naar dit
+**  bestand door editcode te typen en voeg zeker instructies toe.
+**
+**	Good luck !
+**/
+
+void player_function()
+{
+	walk(RIGHT);
+}
