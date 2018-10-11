f7c8741e956f1def83227964a0c0b1e93551c26f
Loic Siquet <lsiquet@w-r5-p3.s19.be>
2018-10-11 15:20:50 +0200

    Updated setlanguage script so it refreshes automatically the level file

--- scripts/setlanguage.sh
+++ scripts/setlanguage.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-__GAME_PATH__
+game_path='/Users/lsiquet/Documents/women-in-tech-c/'
 cd $game_path
 
 echo -n "What language should be installed ? (en/fr) : "
@@ -23,3 +23,8 @@ elif [[ "$language" == "fr" ]]
 then
 	echo "La langue est maintenant configurée en français"
 fi
+
+lvl=$(cat data/current_level)
+
+rm src/player_function.c 2>&1 > /dev/null
+cp data/samples/$language/$lvl.c src/player_function.c 2>&1 > /dev/null
