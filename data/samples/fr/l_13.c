ddcfc3268cc6407d5c3b4ac05cac92a699048444
Loic Siquet <lsiquet@w-r4-p2.s19.be>
2018-10-13 10:23:35 +0200

    Fixed level 13 translation fr sample

--- data/samples/fr/l_13.c
+++ data/samples/fr/l_13.c
@@ -1,26 +1,26 @@
 #include "game_functions.h"
 
-/*
-**	La boucle for fait exactement la meme chose que la boucle while
-**	mais la syntaxe est legerement differente. Elle permet de declarer
-**	votre iterateur, ecrire votre condition et incrementer sur la meme ligne.
+/*	La boucle for fait exactement la meme chose que la boucle while
+**	mais la syntaxe est legerement differente. Elle permet de declarer la
+**	variable incrementee (iterateur), ecrire ta condition et incrementer
+**	l'iterateur sur la meme ligne.
 **
-**	L'ecriture ressemble a :
+**	Tu peux l'ecrire comme ceci :
 **	for (int iterator = 1 ; condition ; iterator++)
 **	{
 **		code a executer
 **	}
 **
-**	Note : iterator++ est une facon d'ecrire plus courte que 
+**	Note : iterator++ est la facon courte d'ecrire
 **		iterator = iterator + 1
-**	Ceci est appeler un operateur d'incrementation.
+**	Ca s'appelle un operateur d'incrementation
 **/
 
 void player_function()
 {
 	for (int iterator = 0 ; iterator <= 20 ; iterator++)
 	{
-		//En utilisant la boucle for vous recuperer 2 lignes de code, interresant non?
-		//il manque sans doute quelque chose pour finir ce niveau.
+		//En utilisant la boucle for, tu peux economiser 2 lignes. Interessant...
+		//Quelque chose manque peut etre au code pour completer le niveau...
 	}
 }
